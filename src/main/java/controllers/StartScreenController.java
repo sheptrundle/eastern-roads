5a27e70d038d568071ebaf0fa7c24ea4e3d901b1
sheptrundle <dvf5rd@virginia.edu>
2025-05-14 16:30:58 -0400

    Added credits

--- src/main/java/controllers/StartScreenController.java
+++ src/main/java/controllers/StartScreenController.java
@@ -1,11 +1,14 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.VBox;
 import models.DatabaseDriver;
 import models.ImageLoader;
 import models.SceneSwitcher;
@@ -13,6 +16,10 @@ import models.SceneSwitcher;
 public class StartScreenController {
     @FXML private AnchorPane anchorPane;
     @FXML private Label message;
+    @FXML private VBox creditsBox;
+    @FXML private Label creditsLabel;
+    @FXML private Button creditsButton;
+    @FXML private Button exitButton;
     private DatabaseDriver db;
 
     public void setDatabaseDriver(DatabaseDriver db) {
@@ -23,13 +30,26 @@ public class StartScreenController {
         ImageLoader imageLoader = new ImageLoader();
         BackgroundImage backgroundImage = imageLoader.loadImage("/images/ERStartScreen.jpg");
         anchorPane.setBackground(new Background(backgroundImage));
+        message.setText("   Welcome to \n Eastern Roads!");
     }
 
-    @FXML public void handleMyCreatures(ActionEvent event) {
+    @FXML
+    public void handleCredits(ActionEvent event) {
+        boolean show = !creditsBox.isVisible();
+        creditsBox.setVisible(show);
+        creditsBox.setManaged(show);
+        creditsLabel.setText("Game developed as a personal project by Shep Trundle,\n" +
+                "undergraduate student at the University of Virginia.\n\n" +
+                "Built using IntelliJ and JavaFX.\n\n" +
+                "Art by Shep Trundle, made using pixilart.com");
+    }
+
+    @FXML public void handleExit(ActionEvent event) {
         try {
-            SceneSwitcher.changeScene("my-creature.fxml", event, "My Creature");
+            db.disconnect();
+            Platform.exit();
         } catch (Exception e) {
-            message.setText("Error loading new scene");
+            message.setText("Error closing application");
         }
     }
 }
